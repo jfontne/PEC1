251b63b5ddd04187cf471a7302f4514697a08c16
Jordi Font Nebot <jfontne@uoc.edu>
2022-10-09 23:13:27 +0200

    v1.1

--- PEC1_Ej2_1/script.js
+++ PEC1_Ej2_1/script.js
@@ -4,22 +4,31 @@ const unitatsOrigen = document.getElementById("amount-one");
 const totalDesti = document.getElementById("amount-two");
 const equivalencia = document.getElementById("rate");
 const intercanvi = document.getElementById("swap");
+const urlAPI = document.getElementById("address");
 
 //Farem un fetch per llegir les dades de les diferents monedes
 
 function calculate() {
     //recuperem les dades introduïdes
+    let mOrigen;
+    let mDesti;
+    let uAPI;
     mOrigen = monedaOrigen.value;
     mDesti=monedaDesti.value;
+    uAPI=urlAPI.value;
 
     //Anem a recuperar les dades de l'API de monedes
-    fetch(`https://v6.exchangerate-api.com/v6/cb7945b00b699f1abb428696/latest/${mOrigen}`)
+    fetch(uAPI + '/' + mOrigen)
         .then(res=>res.json())
         .then(data=>{
             let valor;
             valor=data.conversion_rates[mDesti];
             equivalencia.innerHTML=`1 ${mOrigen} = ${valor} ${mDesti}`
             totalDesti.value=(unitatsOrigen.value*valor).toFixed(2);
+        })
+        .catch(error=>{
+            equivalencia.style.color='#FF0000';
+            equivalencia.innerHTML=error;
         });
 }
 
