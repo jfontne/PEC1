d5c46e55e29b0ba5b1b6f4769734e72929bfc040
Jordi Font Nebot <jfontne@uoc.edu>
2022-10-12 16:46:40 +0200

    v1.3

--- PEC1_Ej2_3/script.js
+++ PEC1_Ej2_3/script.js
@@ -5,13 +5,29 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 //posant un + davant convertim un string en un number 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    console.log(selectedSeats);
+    
+    //Anem a copiar els seients seleccionats
+    //farem un mapa mitjançant un array
+    
+
+
+    const selectSeatsCount = selectedSeats.length;
+    count.innerText = selectSeatsCount;
+    total.innerText = selectSeatsCount * ticketPrice;
 }
 
+//Event de la tria de la peli
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
+
+
+//Events del conainer
 container.addEventListener('click', e => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
         e.target.classList.toggle('selected');
