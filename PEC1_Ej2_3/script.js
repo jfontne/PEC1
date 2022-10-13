c640b000d0593357217f77b074dc056e0ee11e9f
Jordi Font Nebot <jfontne@uoc.edu>
2022-10-13 23:46:34 +0200

    exercici 3

--- PEC1_Ej2_3/script.js
+++ PEC1_Ej2_3/script.js
@@ -4,12 +4,14 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 const moneda = document.getElementById('currency-one');
-var rate;
+const pelis = [];
+const preuPelis = [];
 
 populateUI();
 
+
 //posant un + davant convertim un string en un number 
-let ticketPrice = +movieSelect.value;
+var ticketPrice = +movieSelect.value;
 
 //Guardem la peli seleccionada i el preu
 function setMovieData(movieIndex, moviePrice) {
@@ -35,6 +37,7 @@ function updateSelectedCount() {
 //Consultem les dades desades localment
 function populateUI() {
     const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    let p;
 
     if (selectedSeats !== null && selectedSeats.length > 0) {
         seats.forEach((seat, index) => {
@@ -48,29 +51,54 @@ function populateUI() {
     if (selectedMovieIndex!==null) {
         movieSelect.selectedIndex = selectedMovieIndex;
     }
+
+
+    //Guardem els valors originals del desplegable de pelis
+    for (let i = 0; i<=movieSelect.length-1; i++) {
+        pelis.push(movieSelect.options[i].textContent);
+        preuPelis.push(movieSelect.options[i].value);
+    }
 }
 
 //Event per el canvi de moneda
-moneda.addEventListener('change', e => { 
-    let longitudSelect;
-    
-    longitudSelect=movieSelect.options.length;
-    
-    rateCalculate('USD',e.target.value);
-    if(rate>0){
-        ticketPrice = +movieSelect.value*rate;
-        }
-    for (let i = 0; i <=longitudSelect; i++) {
-        movieSelect.options[i].textContent = +rate;
+moneda.addEventListener('change', e =>{
+        //recuperem les dades introduïdes
+        let uAPI;
+        let mOrigen;
+        let mDesti;
+        mOrigen='USD';
+        mDesti=e.target.value;
+        uAPI=`https://v6.exchangerate-api.com/v6/cb7945b00b699f1abb428696/latest/${mOrigen}`; 
+        //Anem a recuperar les dades de l'API de monedes
+        fetch(uAPI)
+            .then(res=>res.json())
+            .then(data=>{
+                let rate;
+                let nouTextOption;
+                let valorCanvi;
+                rate=data.conversion_rates[mDesti];
+                for (let i = 0; i <=pelis.length-1; i++) {
+                    valorCanvi=+(preuPelis[i]*rate).toFixed(2);
+                    nouTextOption=pelis[i].replace(preuPelis[i],valorCanvi);
+                    nouTextOption=nouTextOption.replace('$', mDesti + ' ');
+                    movieSelect.options[i].textContent = nouTextOption;
+                    movieSelect.options[i].value = valorCanvi;    
+                }
+            
+            ticketPrice = +movieSelect.value;
+            updateSelectedCount();
+                
+            })
+            .catch(error=>{
+                rate=0;
+            });
     }
-    
-});
+);
 
 //Event de la tria de la peli
 movieSelect.addEventListener('change', e => {
-    rateCalculate('USD',moneda.value);
-    if(rate>0){
-        ticketPrice = +e.target.value*rate;}
+    //rateCalculate('USD',moneda.value);
+        ticketPrice = +e.target.value;
         setMovieData(e.target.selectedIndex, e.target.value);
         updateSelectedCount();
 });
@@ -86,23 +114,7 @@ container.addEventListener('click', e => {
 
 });
 
-function rateCalculate(monedaOrigen,monedaDesti) {
-    //recuperem les dades introduïdes
-    var uAPI;
-    var mDesti;
-    mDesti=monedaDesti;
-    uAPI=`https://v6.exchangerate-api.com/v6/cb7945b00b699f1abb428696/latest/${monedaOrigen}`; 
-    //Anem a recuperar les dades de l'API de monedes
-    fetch(uAPI)
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            rate=data.conversion_rates[mDesti];
-        })
-        .catch(error=>{
-            rate=0;
-        });
-}
+
 
 
 
